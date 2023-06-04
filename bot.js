b9c69a6b16f2890a931963a574d2056156b5681b
Sadlyabdullah <135534028+Sadlyabdullah@users.noreply.github.com>
2023-06-04 20:55:14 +0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+const Docker = require('dockerode');
+
+const bot = new Discord.Client();
+const docker = new Docker();
+
+const PREFIX = '!';
+
+bot.on('ready', () => {
+  console.log('Bot is online!');
+});
+
+bot.on('message', (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(PREFIX)) return;
+
+  const args = message.content.slice(PREFIX.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === 'startcontainer') {
+    startContainer(message);
+  } else if (command === 'stopcontainer') {
+    stopContainer(message);
+  }
+});
+
+function startContainer(message) {
+  const containerName = 'mcserver';
+  const container = docker.getContainer(containerName);
+
+  container.start((err) => {
+    if (err) {
+      message.channel.send(`Failed to start container: ${err.message}`);
+    } else {
+      message.channel.send(`Container ${containerName} started successfully!`);
+    }
+  });
+}
+
+function stopContainer(message) {
+  const containerName = 'mcserver';
+  const container = docker.getContainer(containerName);
+
+  container.stop((err) => {
+    if (err) {
+      message.channel.send(`Failed to stop container: ${err.message}`);
+    } else {
+      message.channel.send(`Container ${containerName} stopped successfully!`);
+    }
+  });
+}
+
+bot.login('MTExNDk0MjYzMjk2ODMzNTQwMQ.GgeZjP.V5BMpJibrSsSHmIL-QnberBFRpVvSL08_Vtqrk');
